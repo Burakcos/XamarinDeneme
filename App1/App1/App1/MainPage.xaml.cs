22a81a91131bbac3a2a9dd3547bcea0a7130e081
Burak Coşkun <burakc@otokod.com.tr>
2020-04-15 19:51:48 +0300

    rv2

--- App1/App1/App1/MainPage.xaml.cs
+++ App1/App1/App1/MainPage.xaml.cs
@@ -16,23 +16,49 @@ namespace App1
         public MainPage()
         {
             InitializeComponent();
-            slider.Value = 0.5;
-          
+            //slider.Value = 0.5;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(0, 40, 0, 0);
+                    break;
+                case Device.Android:
+                    this.Padding = new Thickness(0, 10, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+             this.BackgroundColor= Color.FromHex("00bcd4");
+
+            label1.Text = "Purki App";
+            label1.TextColor =Color.White;
+
+            label2.Text = "Hoşgeldin çiçek gibi uygulamaya Çakalll";
+            label2.TextColor =Color.White;
+
+            Button1.Text = "Login";
+            Button1.TextColor = Color.White;
+            Button1.BackgroundColor= Color.FromHex("#dd2c00");
+
+            Button2.Text = "Kayıt";
+            Button2.TextColor = Color.White;
+            Button2.BackgroundColor = Color.FromHex("#dd2c00");
         }
+        
+        //int count = 0;
+        //void Handle_Click(object sender,EventArgs e)
+        //{
+        //    count++;
+        //    var btn = (Button)sender;
+        //    btn.Text = $"butona {count} kez tıklandı.";
 
-        int count = 0;
-        void Handle_Click(object sender,EventArgs e)
-        {
-            count++;
-            var btn = (Button)sender;
-            btn.Text = $"butona {count} kez tıklandı.";
+        //}
 
-        }
+        //void Handle_Slider(object sender,Xamarin.Forms.ValueChangedEventArgs e)
+        //{
+        //    label.Text = String.Format("Value is {0:F2}", e.NewValue);
 
-        void Handle_Slider(object sender,Xamarin.Forms.ValueChangedEventArgs e)
-        {
-            label.Text = String.Format("Value is {0:F2}", e.NewValue);
-
-        }
+        //}
     }
 }
